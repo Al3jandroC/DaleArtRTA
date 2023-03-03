a4811213fb993099a0d72cdc049522bb8e7e3f6b
AlejandroC <alexcastillo@cbluna.com>
2023-03-02 17:58:03 -0800

    Republish v 1.5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,10 +5,10 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "Recurso_9iamgigfastd.png": "918a4fd6c5d2f355f197049df5d7f72b",
 "version.json": "8ace6c06769b6b39f9aa4e0fe2e7d291",
-"index.html": "0c735d59684612e9413dd494a0377a0d",
-"/": "0c735d59684612e9413dd494a0377a0d",
+"index.html": "6ce9b37ad54991b9ec2a6b5caf86a351",
+"/": "6ce9b37ad54991b9ec2a6b5caf86a351",
 "artser_app_2@2x.png": "3ad4b29d8c1e4a35c73f470e88fa0273",
-"main.dart.js": "dc06ac53e901871ece563e2ee89d9a00",
+"main.dart.js": "14eb9771ab516772e044e05217e98859",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
