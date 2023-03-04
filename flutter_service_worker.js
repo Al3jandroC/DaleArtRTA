e55242b95a6c8ea474f18ca466a2b64fe818f5b6
AlejandroC <alexcastillo@cbluna.com>
2023-03-03 18:08:00 -0800

    v 1.5.2 Headers Removed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,10 +5,10 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "Recurso_9iamgigfastd.png": "918a4fd6c5d2f355f197049df5d7f72b",
 "version.json": "8ace6c06769b6b39f9aa4e0fe2e7d291",
-"index.html": "680c7e62c64a11909ecaa9badd81c68f",
-"/": "680c7e62c64a11909ecaa9badd81c68f",
+"index.html": "0ee4da2e746e1d31cf7ba8eadda56364",
+"/": "0ee4da2e746e1d31cf7ba8eadda56364",
 "artser_app_2@2x.png": "3ad4b29d8c1e4a35c73f470e88fa0273",
-"main.dart.js": "9b013d4ac7d24343ac5bf6189e0bd91f",
+"main.dart.js": "8191da0934fba8c24b598ee58572848f",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
